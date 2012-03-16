bbad1f1a854c0c51678867dbe85dc03db557ee64
Shashank Nayak <link4shashank@gmail.com>
2012-03-16 11:35:11 +0530

    first

--- basic/helloworld.py
+++ basic/helloworld.py
@@ -1,5 +1,18 @@
-def hello():
+import sys
+
+def hello(name):
   """The universal first program everyone writes before embarking on a
      programming journey """
-  return "Hello World"
-  
+  print "Hello",name
+
+def main():
+  if not sys.argv[1]:
+    name = "???"
+  else:
+    name = sys.argv[1]
+  hello(name)
+
+#Boilerplate
+
+if __name__ == "__main__":
+  main()
